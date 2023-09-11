8a4ce075bccd277dc14a3602583f7b4a867843b7
l.minye@digipen.edu <l.minye@digipen.edu>
2023-09-11 20:46:03 +0800

    adding football game

---
+++ Football/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1852920.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 149254873, 149298784, 149321756, 149346619 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/149248396/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/149248397/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/149248395/1/ammo.js', 'preload' : true},
+];
